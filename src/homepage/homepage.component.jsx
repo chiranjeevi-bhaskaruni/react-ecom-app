bf38ba986f1559270740491df2e93a17197a18c9
chiranjeevi-bhaskaruni <bhaskarunichiranjeevi@gmail.com>
2020-05-25 11:41:39 -0500

    simplified homepage to components and applied component level styles

---
+++ src/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './homepage.component.scss';
+import Menu from './components/menu/menu.component'
+
+export const HomePage = () => {
+    return (
+        <div className="homepage">
+            <Menu />
+        </div>
+    )
+};
